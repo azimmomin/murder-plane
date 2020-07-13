93048cb464d2c4a719d2a23cb8ef5fc35d58aa53
azimmomin <3822259+azimmomin@users.noreply.github.com>
2020-07-12 17:23:44 -0700

    Another round of deletion and cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,39 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-///<summary>
+/// <summary>
 /// This class moves the player based on user input. 
-///</summary>
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] private PlayerInputManager playerInputManager = null;
   [SerializeField] private float speed = 5f;
   [SerializeField] private float rotationSpeed = 0.2f;
-  // If true, the player will pitch upwards when the
-  // user swipes up and downwards when the user swipes
-  // down. If false, the player will pitch in the
-  // opposite direction of the swipe.
-  [SerializeField] private bool useNaturalMotion = true;
   [SerializeField] private float minRotationAngle = -30;
   [SerializeField] private float maxRotationAngle = 50f;
+  /// <summary>
+  /// If true, the player will pitch upwards when the
+  /// user swipes up and downwards when the user swipes
+  /// down. If false, the player will pitch in the
+  /// opposite direction of the swipe.
+  /// </summary>
+  [SerializeField] private bool useNaturalMotion = true;
 
-  void Update()
+  private bool shouldStopMovement = false;
+  private void Update()
   {
-    // Move the plane forward at a constant speed
+    // Move the player forward at a constant speed
+    if (!shouldStopMovement)
     transform.Translate(0f, 0f, speed * Time.deltaTime);
 
-    Vector2 delta  = playerInputManager.GetChangeInPlayerInput();
+    // Rotate the player based on user input.
+    Vector2 delta = playerInputManager.GetChangeInPlayerInput();
     transform.Rotate(Vector3.up * (delta.x * rotationSpeed * Time.deltaTime));
     float direction = useNaturalMotion ? -1f : 1f;
     transform.Rotate(Vector3.right * (delta.y * rotationSpeed * Time.deltaTime * direction));
   }
 
-  void LateUpdate()
+  private void LateUpdate()
   {
-    // Clamp the player's rotation so they can't do back flips.
+    // Clamp the player's rotation to the specified range.
     Vector3 currentRotation = transform.eulerAngles;
     currentRotation.x = MathfExtensions.ClampAngle(currentRotation.x, minRotationAngle, maxRotationAngle);
     currentRotation.z = 0f;
     transform.rotation = Quaternion.Euler(currentRotation);
+  }
+
+  private void OnCollisionEnter(Collision collisionInfo)
+  {
+    shouldStopMovement = true;
+    Debug.LogError($"Trigger Entered -- {collisionInfo.collider.gameObject.tag}");
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    shouldStopMovement = true;
+    Debug.LogError($"Trigger Entered -- {other.gameObject.tag}");
   }
 }
